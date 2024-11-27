2e3daef5db36c6f3184c834fa5361c99111dea92
Zul Qarnain Afridi | MERN Stack Developer <beingzulqarnain@gmail.com>
2024-11-27 23:59:57 +0500

    Let's learn JavaScript Variables & DataTypes | Episode -2

---
+++ Complete Development Practice/1.FRONT-END/3.JAVASCRIPT/Episode-2/script.js
@@ -0,0 +1,167 @@
+// Introduction of JS Courses
+/*
+console.log("hello friends");
+console.log("Welcome To JS Courses");
+console.log(34 + 45);
+console.log(true);
+console.log("Hello Worlds" + 123);
+console.log(null);
+console.log(a=4);
+*/
+
+//Js Essentials (Variables)
+//  var let const 
+
+// variables
+/*
+var fullName = "Afridi";
+var age = 34;
+var isFollow = false;
+var data = BigInt(23)
+var data2 = Symbol(32)
+console.log(data2);
+*/
+
+//let 
+/*
+let age = 34;
+age =45;
+
+let a;
+let values = null;
+// console.log(values);
+let fullName = "Twitter";
+let isFollow = true;
+isFollow = false;
+// console.log(isFollow);
+
+
+let rollNumber = 34;
+console.log(rollNumber + " this is rollnumber");
+*/
+
+
+//Const
+/*
+const fullName = "Qarnain Afridi";
+//   fullName = "khan"; to shows an error because of constant variables.
+const price = 43.99;
+const  deal = true;
+console.log(deal);
+const age = 22;
+console.log(age);
+const classmates = 45;
+console.log(classmates);
+//Variables Declaration and Initialization Process
+const $fullName = "Imran Khan";
+console.log($fullName);
+const _rollNumber = 1234523;
+console.log(_rollNumber);
+//Reserved Keywords don't used in JS
+const console = "Cricket";
+console.log(console);
+*/
+
+//Objects
+/*
+const person = {
+    firstName : "Zul",
+    lastName : "Qarnain",
+    age : 34,
+    isMarried:true,
+    city : "Peshawar"
+}
+
+person["age"] = 55;
+
+// console.log(person["firstName"]);
+// console.log(person["age"]);
+// person.cgpa = 3.45;
+// console.log(person.firstName);
+// console.log(person.lastName);
+// console.log(person.age);
+// console.log(person.isMarried);
+// console.log(person.city);
+// console.log(person.cgpa);
+
+//02 Object of Products
+const product = {
+    Title: "Baby Wipes",
+    Colors:"red",
+    Price:100
+};
+
+// console.log(product["Title"],product["Price"]);
+
+//o3 Objects of Profile
+const Profile = {
+    userName : "@beingzulqarnain",
+    post:12,
+    followers:234,
+    following:500,
+    isFollow: true
+};
+
+console.log(Profile.userName);
+console.log(Profile.post);
+console.log(Profile.followers);
+console.log(Profile["isFollow"]);
+*/
+
+//object
+
+// const roll = {
+//     name: 'kabir ahmad',
+//     roll:34,
+//     age:24,
+
+// };
+// console.log(roll);
+
+//bigint variable
+// let big = BigInt(234);
+// console.log(big);
+
+//Symbol variable
+// let skill = Symbol("code thread")
+// console.log(typeof skill);
+
+//undefined
+
+// let b; 
+// console.log(b);
+
+// let a = null;
+// console.log(typeof a);
+
+// let boolean = true;
+// console.log(typeof boolean);
+
+
+// let str = "codeThread";
+// console.log( typeof str);
+
+
+// let num = 45;
+// console.log(typeof (num));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
