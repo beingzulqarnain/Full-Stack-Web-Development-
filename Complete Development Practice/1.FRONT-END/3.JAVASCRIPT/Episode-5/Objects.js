9f7efaa1042cfb0cdb5d17e772f7b91b974fa015
Zul Qarnain Afridi | MERN Stack Developer <beingzulqarnain@gmail.com>
2024-11-29 01:22:10 +0500

    Let's learn JavaScript Arrays & Objects & Methods | Episode -5

---
+++ Complete Development Practice/1.FRONT-END/3.JAVASCRIPT/Episode-5/Objects.js
@@ -0,0 +1,142 @@
+
+
+// //Objects
+
+// // const studentDetails = {
+// //     firstName : "Haroon",
+// //     lastName  : "Shah",
+// //     id        : 234524 ,
+// //     cityName  : "Peshawar"
+// // };
+
+// // console.log(studentDetails["firstName"]);
+// // console.log(studentDetails["lastName"]);
+
+
+
+// let studentRecord = [
+//     {
+//             firstName : "Haroon",
+//             lastName  : "Shah",
+//             id        : 234524 ,
+//             cityName  : "Peshawar"
+//     },
+//     {
+//         firstName : "Haroon",
+//         lastName  : "Shah",
+//         id        : 234524 ,
+//         cityName  : "Peshawar"
+// },
+// {
+//     firstName : "Haroon",
+//     lastName  : "Shah",
+//     id        : 234524 ,
+//     cityName  : "Peshawar"
+// },
+// {
+//     firstName : "Haroon",
+//     lastName  : "Shah",
+//     id        : 234524 ,
+//     cityName  : "Peshawar"
+// },
+// {
+//     firstName : "Haroon",
+//     lastName  : "Shah",
+//     id        : 234524 ,
+//     cityName  : "Peshawar"
+// },
+// {
+//     firstName : "Haroon",
+//     lastName  : "Shah",
+//     id        : 234524 ,
+//     cityName  : "Peshawar"
+// },
+// {
+//     firstName : "Haroon",
+//     lastName  : "Shah",
+//     id        : 234524 ,
+//     cityName  : "Peshawar"
+// },
+// {
+//     firstName : "Haroon",
+//     lastName  : "Shah",
+//     id        : 234524 ,
+//     cityName  : "Peshawar"
+// },
+// ];
+
+// console.log(studentRecord);
+
+
+
+// let arr = [1,2,3,4,5,6,7,8,9];
+
+// for(let value of arr){
+//     console.log(value);
+// }
+
+
+
+// const studentDetails = {
+//     firstName : "Haroon",
+//     lastName  : "Shah",
+//     id        : 234524 ,
+//     cityName  : "Peshawar"
+// };
+
+
+// for(const val in studentDetails){
+//     console.log(studentDetails[val]);
+// }
+
+
+// Spread Operator
+
+// let arr1 = [1,2,3,4,5];
+// let arr2 = [6,7,8,9,10];
+
+// let newArray = [5,6,7,8,...arr2];
+
+// console.log(newArray);
+
+
+// Rest Operator 
+// let [one ,two ,three,four,...hello] = arr1
+
+// console.log(hello);
+
+
+// Object Destructuring 
+
+
+// const studentDetails = {
+//         firstName : "Haroon",
+//         lastName  : "Shah",
+//         id        : 234524 ,
+//         cityName  : "Peshawar",
+//         Courses : "c++"
+//     };
+
+
+//     let {firstName,lastName,id,cityName,...hello} =studentDetails;
+//     console.log(hello);
+   //Spread Operator in Objects 
+//  const studentDetails = {
+//             firstName : "Haroon",
+//             lastName  : "Shah",
+//             id        : 234524 ,
+//             cityName  : "Peshawar",
+//             Courses : "c++"
+//         };
+//  const studentDetails1 = {
+//             firstName : "Haroon",
+//             lastName  : "Shah",
+//             id        : 234524 ,
+//             cityName  : "Peshawar",
+//             Courses : "c++"
+//         };
+
+
+//         let newObj = {studentDetails,studentDetails1};
+//         console.log(newObj);
+        
---
+++ Complete Development Practice/1.FRONT-END/3.JAVASCRIPT/Episode-5/arrays.js
@@ -0,0 +1,140 @@
+
+
+// arr.push([34,45,66,22],"hello",true);
+// arr.pop(); deleting items from ending
+// arr.shift(); from starting add numbers
+// arr.unshift(8,9,10); from ending it can added number
+
+// let newArray = arr.concat(4,3,6,2);
+// let newArray = arr.join('-');
+// let newArray = arr.indexOf(4); searching 4 is in which index store.
+// let newArray = arr.lastIndexOf(5);
+// let newArray = arr.length; length find the array length
+// let newArray = arr.reverse(); reverse works to change asc to decending order
+// arr.sort();
+//slice 
+// let arr = [1,2,3,4,5,6];
+// let newArray = arr.slice(1,4);
+// console.log(newArray);
+
+//splice
+// let arr = [1,2,3,4,5,6];
+// let newArray = arr.splice(1,4);
+// console.log(newArray);
+// console.log(arr);
+
+// let arr = [1,2,3,4,5,6];
+// console.log(arr[0]);
+// console.log(arr[1]);
+// console.log(arr[2]);
+// console.log(arr[3]);
+// console.log(arr[4]);
+// console.log(arr[5]);
+
+// let arr = [1,2,3,4,5,6];
+// for(let i =0; i<arr.length; i++){
+    //     console.log(arr[i]);  
+    // }
+
+//  let arr = [1,2,3,4,5,6];
+
+//  let [one,two,three,four,five,six] = arr;
+// //destructuring of array
+// console.log(one);
+// console.log(two);
+// console.log(three);
+// console.log(four);
+// console.log(five);
+// console.log(six);
+
+
+
+
+
+
+
+// let arr = [22,"Ahmad",true,{firstName:"Zulqarnain"},"Cecos"];
+
+// console.log(arr);
+// console.log(arr[4]);
+// console.log(arr[3]);
+
+
+
+// let languages = ["JAVA","CPP","JAVASCRIPT","PYTHON","RUBY","C","C#"];
+
+// console.log(languages);
+// console.log(languages[0]);
+// console.log(languages[1]);
+// console.log(languages[2]);
+// console.log(languages[3]);
+
+
+
+
+
+
+// let arr = [23,44,55,63,72,88,11,27];
+
+// console.log(typeof arr);
+// console.log(arr[0]);
+// console.log(arr[1]);
+// console.log(arr[2]);
+// console.log(arr[3]);
+// console.log(arr[4]);
+
+
+//Arrays 
+// let arr = [1,2,3,4,5];
+
+// for(let i = 0;  i<arr.length; i++){
+//   console.log(`arr[${i}] =  ${arr[i]}`);
+// }
+
+// console.log("arr[0] = ",arr[0]);
+// console.log("arr[1] = ",arr[1]);
+// console.log("arr[2] = ",arr[2]);
+// console.log("arr[3] = ",arr[3]);
+// console.log("arr[4] = ",arr[4]);
+
+// let cities = ["Peshawar","Karachi","Lahore","Islamabad","Multan","Gujranawala"];
+//For Loop
+// for(i=0;  i<cities.length; i++){
+//     console.log(`cities ${[i]} = ${cities[i]}`);
+// }
+
+//of Loop
+// for(let city of cities){
+//     console.log(city);
+// }
+
+// let marks_of_Student = [85,97,44,37,76,60];
+// let sum = 0;
+// for(let i =0; i<marks_of_Student.length; i++){
+//     sum = sum + marks_of_Student[i];
+//     console.log(marks_of_Student[i]);
+// }
+
+// let avg = sum/marks_of_Student.length;
+// console.log(avg);
+
+// let items = [250,645,300,900,50];
+// for(let i=0; i<items.length; i++)
+// {
+//     let offer = items[i]/10;
+//     items[i] = items[i] - offer;
+// }
+// console.log(`value after offer is ${items}`);
+
+
+
+
+// let i = 0;
+// for(let val of items){
+//    let  offer = val / 10;
+//     items[i] = items[i] - offer;
+//     console.log(`value after offer is ${items[i]}`);
+//     i++;
+// }
+// console.log(items);
+
